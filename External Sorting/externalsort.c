992ed19adf1729325714427fe0027f5ea21b18cb
neloydutta <neloy.dutta@live.com>
2017-01-31 16:57:29 +0530

    External Sort update

--- External Sorting/externalsort.c
+++ External Sorting/externalsort.c
@@ -3,8 +3,7 @@
 #include <limits.h>
 #include <unistd.h>
 
-void merge(int arr[], int l, int m, int r)
-{
+void merge(int arr[], int l, int m, int r){
     int i, j, k;
     int n1 = m - l + 1;
     int n2 =  r - m;
@@ -16,38 +15,31 @@ void merge(int arr[], int l, int m, int r)
     i = 0;
     j = 0;
     k = l;
-    while (i < n1 && j < n2)
-    {
-        if (L[i] <= R[j])
-        {
+    while (i < n1 && j < n2){
+        if (L[i] <= R[j]){
             arr[k] = L[i];
             i++;
         }
-        else
-        {
+        else{
             arr[k] = R[j];
             j++;
         }
         k++;
     }
-    while (i < n1)
-    {
+    while (i < n1){
         arr[k] = L[i];
         i++;
         k++;
     }
-    while (j < n2)
-    {
+    while (j < n2){
         arr[k] = R[j];
         j++;
         k++;
     }
 }
 
-void mergeSort(int arr[], int l, int r)
-{
-    if (l < r)
-    {
+void mergeSort(int arr[], int l, int r){
+    if (l < r){
         int m = l+(r-l)/2;
         mergeSort(arr, l, m);
         mergeSort(arr, m+1, r);
@@ -69,50 +61,69 @@ int pick_minimum(int *arr, int size){
 void merge_files(int size, char *outfile){
     int i, num[size], min_index, eof_count = 0;
     FILE *outputfile = fopen(outfile,"w");
-    FILE *intermediate_outfile[size];
+    FILE *intermediate_infile[500];
+    FILE *intermediate_outfile;
     char temp[100];
-
-    for(i=0; i<size; i++){
-        sprintf(temp,"out%d.txt",i+1);
-        intermediate_outfile[i] = fopen(temp,"r");
-        if(!feof(intermediate_outfile[i])){
-            fscanf(intermediate_outfile[i],"%d",&num[i]);
+    int level = 0;
+    int intermediate_outfile_count = size;
+    int completed_file_count = 0;
+    while(intermediate_outfile_count > 1){
+        intermediate_outfile_count = 0;
+        while(completed_file_count < size){
+            sprintf(temp, "out%d%d.txt", level+1, intermediate_outfile_count++);
+            intermediate_outfile = fopen(temp, "w");
+            for(i=0; i<500 && i<size; i++){
+                sprintf(temp,"out%d%d.txt",level,i+1);
+                intermediate_infile[i] = fopen(temp,"r");
+                completed_file_count += 1;
+                if(!feof(intermediate_infile[i])){
+                    fscanf(intermediate_infile[i],"%d",&num[i]);
+                }
+                else{
+                    num[i] = INT_MAX;
+                    eof_count += 1;
+                }
+            }
+            while(eof_count < completed_file_count){
+                min_index = pick_minimum(num, size);
+                fprintf(intermediate_outfile, "%d\n", num[min_index]);
+                if(!feof(intermediate_infile[min_index])){
+                    fscanf(intermediate_infile[min_index],"%d",&num[min_index]);
+                }
+                else{
+                    num[min_index] = INT_MAX;
+                    eof_count += 1;
+                    fclose(intermediate_infile[min_index]);
+                    sprintf(temp,"out%d%d.txt", level, min_index+1);
+                    unlink(temp);
+                }
+            }
+            fclose(intermediate_outfile);
         }
-        else{
-            num[i] = INT_MAX;
-            eof_count += 1;
-        }
+        completed_file_count = 0;
+        level += 1;
     }
-    while(eof_count < size){
-        min_index = pick_minimum(num, size);
-        fprintf(outputfile, "%d\n", num[min_index]);
-        if(!feof(intermediate_outfile[min_index])){
-            fscanf(intermediate_outfile[min_index],"%d",&num[min_index]);
-        }
-        else{
-            num[min_index] = INT_MAX;
-            eof_count += 1;
-        }
+    sprintf(temp, "out%d%d.txt", level, 0);
+    intermediate_outfile = fopen(temp, "r");
+    while((size=fread(temp,1,100,intermediate_outfile)) != 0){
+        fwrite(temp, 1, size, outputfile);
     }
-    for(i=0; i<size; i++){
-        fclose(intermediate_outfile[i]);
-        sprintf(temp,"out%d.txt",i+1);
-        unlink(temp);
-    }
+    sprintf(temp, "out%d%d.txt", level, 0);
+    unlink(temp);
     fclose(outputfile);
     return;
 }
 
-void externalsort(char *input_filename, char *output_filename, unsigned long long max_run_size, unsigned long long num_of_blocks){
+void externalsort(char *input_filename, char *output_filename, unsigned long long max_run_size){
     int i,num, *arr,size, outfile_index;
     char temp[100];
     FILE *ip = fopen(input_filename, "r");
-    FILE *intermediate_outfiles[num_of_blocks];
+    FILE *intermediate_outfile;
     arr = malloc(max_run_size*sizeof(int));
     outfile_index = 0;
-    while(!feof(ip) && outfile_index < num_of_blocks){
-        sprintf(temp, "out%d.txt", outfile_index+1);
-        intermediate_outfiles[outfile_index] = fopen(temp,"w");
+    while(!feof(ip)){
+        sprintf(temp, "out0%d.txt", outfile_index+1);
+        intermediate_outfile = fopen(temp,"w");
         for(i=0;i<max_run_size;i++){
             fscanf(ip,"%d",&num);
             arr[i] = num;
@@ -122,21 +133,19 @@ void externalsort(char *input_filename, char *output_filename, unsigned long lon
         size = i;
         mergeSort(arr,0,size-1);
         for(i=0; i<size; i++)
-            fprintf(intermediate_outfiles[outfile_index],"%d\n",arr[i]);
-        fclose(intermediate_outfiles[outfile_index]);
+            fprintf(intermediate_outfile,"%d\n",arr[i]);
+        fclose(intermediate_outfile);
         outfile_index += 1;
     }
     fclose(ip);
-    free(intermediate_outfiles);
     merge_files(outfile_index, output_filename);
     return;
 }
 
 int main(){
     unsigned long long int max_run_size = 500000;
-    unsigned long long int num_of_blocks = 500;
     char *input_filename = "file.txt";
     char *output_filename = "out.txt";
-    externalsort(input_filename, output_filename, max_run_size, num_of_blocks);
+    externalsort(input_filename, output_filename, max_run_size);
     return 0;
 }
